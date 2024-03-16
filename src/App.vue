4c5f8a851816df10d6a43a8482965a0e2cf045a2
Yo <yonansmillion29@example.com>
2024-03-16 11:38:36 +0300

    pinia logo is created and header part is in building

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
-<script setup>
-</script>
-
 <template>
-  <div class="">hello</div>
+  <main>
+    <!-- heading   -->
+    <header>
+      <img src="./assets/pinia.png" width="40" alt="">
+      <h1 class="">Pinia Tasks</h1>
+    </header>
+
+  </main>
 </template>
+
+<script>
+  export default {
+    
+  }
+</script>
