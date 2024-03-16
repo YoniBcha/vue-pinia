0383ec1f848695a0e428ae8b35a6723a485a5959
Yo <yonansmillion29@example.com>
2024-03-16 11:50:22 +0300

    header is in progress

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,14 @@
   <main>
     <!-- heading   -->
     <header>
-      <img src="./assets/pinia.png" width="40" alt="">
-      <h1 class="">Pinia Tasks</h1>
+      <div class="flex items-center h-16 bg-[#efefef] justify-center">
+        <img src="./assets/pinia.png" width="40" alt="" />
+        <h1 class="font-bold text-xl ml-10">Pinia Tasks</h1>
+      </div>
     </header>
-
   </main>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {};
 </script>
