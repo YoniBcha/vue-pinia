7f16e43297c955279b80d37843af39de38bb807b
Yo <yonansmillion29@example.com>
2024-03-16 21:36:52 +0300

    first pinia code is made

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,30 @@
     <!-- heading   -->
     <header>
       <div class="flex items-center h-16 bg-[#efefef] justify-center">
-        <img class="rotate-[-30deg]" src="./assets/pinia.png" width="40" alt="" />
+        <img
+          class="rotate-[-30deg]"
+          src="./assets/pinia.png"
+          width="40"
+          alt=""
+        />
         <h1 class="font-bold text-xl ml-5 rotate-2">Pinia Tasks</h1>
       </div>
     </header>
+    <!-- task list -->
+    <div class="">
+      <div v-for="task in taskStore.tasks" :key="task.id">
+        <p>{{ task.title }}</p>
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
-export default {};
+import { useTaskStore } from "./stores/TaskStore";
+export default {
+  setup() {
+    const taskStore = useTaskStore();
+    return { taskStore };
+  },
+};
 </script>
